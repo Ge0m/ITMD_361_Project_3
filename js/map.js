f92a5dd934429b38365085acb009cfc94d411763
Ge0m <geldi.omeri@gmail.com>
2018-04-18 21:28:45 -0500

    added map and location
    added map with location set to saranda, albania

--- js/map.js
+++ js/map.js
@@ -1,2 +1,22 @@
 var map;
 function initMap()
+{
+  map = new google.maps.Map(document.getElementById('map'), 
+    {
+      center: {lat: 39.859212, lng: 20.0271},
+      zoom: 15,
+      mapTypeId: 'roadmap',
+      mapTypeControl: true,
+      mapTypeControlOptions: 
+      {
+        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+        mapTypeIds: ['roadmap', 'satellite'],
+        position: google.maps.ControlPosition.TOP_CENTER
+      },
+      zoomControl: true,
+      zoomControlOptions: 
+      {
+        position: google.maps.ControlPosition.RIGHT_CENTER
+      }
+    });
+}
